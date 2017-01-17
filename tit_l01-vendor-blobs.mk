a601278cc62d54f3f6c7573b9b70adca8600d360
igor <igorkud00@gmail.com>
2017-01-17 12:25:18 +0300

    Init

---
+++ tit_l01-vendor-blobs.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+  $(call find-copy-subdir-files,*,vendor/honor/tit_l01/proprietary,system)
+
---
+++ tit_l01-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/honor/tit_l01/tit_l01-vendor-blobs.mk)
